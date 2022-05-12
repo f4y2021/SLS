aa7e9a7287566fb9bc0e52191d03db0373707e80
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-12 16:31:15 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -17,11 +17,13 @@ first_choice = st.selectbox('Select Sample #1',('C11', 'C12', 'C21', 'C22'))
 second_choice = st.selectbox('Select Sample #2',('C11', 'C12', 'C21', 'C22'))
 graph_select=first_choice+"_"+second_choice
 st.write('You selected:', graph_select)
+
+excel_data="SLS_Adhesive_Results3.xlsx"
 run_button=st.button("Run")
 
 if run_button:
 
-  df_aux=pd.read_excel("SLS_Adhesive_Results3.xlsx",sheet_name=graph_select,usecols= [0,1,3,4,6,7,9,10,12,13],header=0,
+  df_aux=pd.read_excel(excel_data,sheet_name=graph_select,usecols= [0,1,3,4,6,7,9,10,12,13],header=0,
                        names=["S1_d","S1_f","S2_d","S2_f","S3_d","S3_f","S4_d","S4_f","S5_d","S5_f"])
 
   df_chosen=df_aux.dropna()
