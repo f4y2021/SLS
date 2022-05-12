7281144a084ef3117c6455722d73c2015addbda5
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-12 16:20:06 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
+"""THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
 
-# In[1]:
+import streamlit as st
 
-
-import os
-os.chdir("C:/dataset")
-
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -18,53 +17,24 @@ from plotly.subplots import make_subplots
 import re
 import sys
 
-
-# In[2]:
-
-
-dfname=pd.ExcelFile("SLS_Adhesive_Results3.xlsx")
-print(dfname.sheet_names)
-
-
-# In[46]:
-
-
-first_choice="C11"
-second_choice="C11"
+first_choice = st.selectbox('Select Sample #1',('C11', 'C12', 'C21', 'C22'))
+second_choice = st.selectbox('Select Sample #2',('C11', 'C12', 'C21', 'C22'))
 graph_select=first_choice+"_"+second_choice
-print(graph_select)
-
-
-# In[47]:
 
 
 df_aux=pd.read_excel("SLS_Adhesive_Results3.xlsx",sheet_name=graph_select,usecols= [0,1,3,4,6,7,9,10,12,13],header=0,
                      names=["S1_d","S1_f","S2_d","S2_f","S3_d","S3_f","S4_d","S4_f","S5_d","S5_f"])
 
-
-# In[48]:
-
-
 df_chosen=df_aux.dropna()
-print(df_chosen)
+st.dataframe(df_chosen)
+
+#ax = plt.gca()
+#for i in range (1,6):
+    #df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
 
 
-# In[49]:
+#st.line_chart(chart_data)
 
-
-ax = plt.gca()
-for i in range (1,6):
-    df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
-plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
