5763cb3a712991a4ead4e255a3756ce5d7f50c0b
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-13 11:25:38 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -34,8 +34,8 @@ if run_button:
   df_chosen=df_aux.dropna()
   st.dataframe(df_chosen)
   fig = px.line(df_chosen, x=df_chosen.columns[0], y=df_chosen.columns[1])
-  for i in range (3,13,3):
-    fig.add_trace(go.Scatter(x="df_chosen.columns[i]", y="df_chosen.columns[i+1]"))
+  #for i in range (3,13,3):
+    #fig.add_trace(px.line(x=df_chosen.columns[i], y=df_chosen.columns[i+1]))
 
   st.plotly_chart(fig)
 
