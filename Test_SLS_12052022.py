37257f16df7703d4698846c985276989bd63ab23
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-13 09:54:09 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -37,13 +37,8 @@ if run_button:
   #fig=df_chosen.plot(x="S1_d",y="S1_f")
   #st.pyplot(fig) 
 
-  
-  plost.line_chart(
-    df_chosen,
-    x='S1_d',
-    y=('S1_f'),
-    pan_zoom='minimap',
-  )
+  for i in range (1,6):
+    plost.line_chart(df_chosen,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
 #for i in range (1,6):
     #df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
 
