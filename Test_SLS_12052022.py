69dc8843aabe3cbf77055cd7676a1c58cc608e15
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-13 09:36:22 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -30,7 +30,11 @@ if run_button:
 
   df_chosen=df_aux.dropna()
   st.dataframe(df_chosen)
+  ax = plt.gca()
+  for i in range (1,6):
+    fig=df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
 
+  st.pyplot(fig)
 #ax = plt.gca()
 #for i in range (1,6):
     #df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
