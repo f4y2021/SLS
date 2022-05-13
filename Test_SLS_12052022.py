4be1479840e2741d8a924d378455b13fd883107b
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-13 11:52:41 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -38,8 +38,8 @@ if run_button:
   #fig.add_trace(go.scatter(x=df_chosen.columns[3], y=df_chosen.columns[4]))
   ax = plt.gca()
   for i in range (1,6):
-    fig=df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
-  st.plotly_chart(fig)
+    slschart=df_chosen.plot(ax=ax,x="S"+str(i)+"_d",y="S"+str(i)+"_f")
+  st.pyplot(fig=slschart)
   #for i in range (3,13,3):
     #fig.add_trace(px.line(x=df_chosen.columns[i], y=df_chosen.columns[i+1]))
 
