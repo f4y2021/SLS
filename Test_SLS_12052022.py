0bd805617f9b5d95fdd59bdda7d259596d9ffd7a
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-16 15:37:05 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -47,9 +47,9 @@ with col22:
 
 col111, col222= st.columns(2)
 with col111:
-    connection_choice = st.selectbox('Select connection technology',('Bolt', 'Adhesive','Hybrid'),disabled=connection_tech)
+    connection_choice = st.selectbox('Select connection technology',('Bolt', 'Adhesive','Hybrid'))
 with col222:
-    connection_tech = st.checkbox('I dont know the connection technology I want to use')
+    connection_tech = st.checkbox('Don''t know')
 
 
 
