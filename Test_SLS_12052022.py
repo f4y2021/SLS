1b1e45ad6def536bfd835bde9c9895ab5ce6ccd5
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2022-05-16 16:14:37 +0100

    Update Test_SLS_12052022.py

--- Test_SLS_12052022.py
+++ Test_SLS_12052022.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 
 import streamlit as st
 
+from docx import Document
+
 import numpy as np
 import pandas as pd
 
@@ -57,28 +59,28 @@ run_button=st.button("Run")
 
 if run_button:
 
-  df_aux=pd.read_excel(excel_file_loc,sheet_name=graph_select,header=0,names=["S1_d","S1_f","S2_d","S2_f","S3_d","S3_f","S4_d","S4_f","S5_d","S5_f"])
+    df_aux=pd.read_excel(excel_file_loc,sheet_name=graph_select,header=0,names=["S1_d","S1_f","S2_d","S2_f","S3_d","S3_f","S4_d","S4_f","S5_d","S5_f"])
 
-  df_chosen=df_aux.dropna()
-  st.dataframe(df_chosen)
-  avg_max_load=(df_chosen["S1_f"].max()+df_chosen["S2_f"].max()+df_chosen["S3_f"].max()+df_chosen["S4_f"].max()+df_chosen["S5_f"].max())/5
-  avg_max_disp=(df_chosen["S1_d"].max()+df_chosen["S2_d"].max()+df_chosen["S3_d"].max()+df_chosen["S4_d"].max()+df_chosen["S5_d"].max())/5
-  avg_lap_shear=(df_chosen["S1_f"].max()+df_chosen["S2_f"].max()+df_chosen["S3_f"].max()+df_chosen["S4_f"].max()+df_chosen["S5_f"].max())/(5*area)
-  fig = go.Figure()
+    df_chosen=df_aux.dropna()
+    st.dataframe(df_chosen)
+    avg_max_load=(df_chosen["S1_f"].max()+df_chosen["S2_f"].max()+df_chosen["S3_f"].max()+df_chosen["S4_f"].max()+df_chosen["S5_f"].max())/5
+    avg_max_disp=(df_chosen["S1_d"].max()+df_chosen["S2_d"].max()+df_chosen["S3_d"].max()+df_chosen["S4_d"].max()+df_chosen["S5_d"].max())/5
+    avg_lap_shear=(df_chosen["S1_f"].max()+df_chosen["S2_f"].max()+df_chosen["S3_f"].max()+df_chosen["S4_f"].max()+df_chosen["S5_f"].max())/(5*area)
+    fig = go.Figure()
 
-  for i in range (1,6):
+    for i in range (1,6):
     col_name_d = "S"+str(i)+"_d"
     col_name_f = "S"+str(i)+"_f"
     fig.add_trace(go.Scatter(x=df_chosen[col_name_d], y=df_chosen[col_name_f],
                   mode='lines',name="S"+str(i)))
-  fig.update_layout(title='SLS Results',
+    fig.update_layout(title='SLS Results',
                    xaxis_title='Displacement (mm)',
                    yaxis_title='Load (N)',template="seaborn")
-  st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
-  col1, col2, col3 = st.columns(3)
-  col1.metric("Average Max Load (N)", round(avg_max_load, 2))
-  col2.metric("Average Max Displacement (mm)",round(avg_max_disp, 2))
-  col3.metric("Average Lap Shear Strength (MPa)",round(avg_lap_shear, 2))
-
-
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Average Max Load (N)", round(avg_max_load, 2))
+    col2.metric("Average Max Displacement (mm)",round(avg_max_disp, 2))
+    col3.metric("Average Lap Shear Strength (MPa)",round(avg_lap_shear, 2))
+    
+    
